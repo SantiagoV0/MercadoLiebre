0fd9228231621a71489f266025a76527b3ea8237
SantiagoV0 <sxd292@gmail.com>
2023-08-12 19:36:13 -0300

    Update css

--- src/app.js
+++ src/app.js
@@ -26,6 +26,6 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"./views/home.html"));
 });
 
-app.get("/contacto",(req,res) => {
-    res.sendFile(path.join(__dirname,"./views/contacto.html"));
-});
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
