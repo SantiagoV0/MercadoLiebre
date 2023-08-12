3449b8323f2eb761827ea41802cd2e5026c3d979
SantiagoV0 <sxd292@gmail.com>
2023-08-12 15:52:25 -0300

    Update json

--- src/app.js
+++ src/app.js
@@ -6,14 +6,20 @@ const app = express();
 
 const path = require("path")
 
+//Variable de entorno, si no, se usa el 3001
+const port = process.env.PORT || 3001;
+
 // 2. Poner a escuchar el servidor
 
-app.listen(3000, () => console.log("Servidor escuchando en el puerto 3000!"));
+app.listen(PORT, () => console.log("Servidor escuchando en el puerto 3000!"));
 
 
 //2.1 definir donde se encuentran nuestros archivos estaticos
 
 app.use(express.static("public"));
+
+
+
 
 // 3. Crear rutas
 app.get("/", (req,res) => {
